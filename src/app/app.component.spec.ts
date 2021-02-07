677cb325688172b95d6bf743c19faa6a34f28df1
Brian Chirgwin <brian@chirgwin.net>
2021-02-07 12:22:30 -0600

    Add deps (#2)
    * build: ng add @briebug/jest-schematic
    
    add jest schematic for jest unit testing
    
    * build: prettier added
    
    vscode prettier extension recommendation for vscode
    set project to autoformat using prettier
    
    * build: add npm run format command
    
    npm run format will run prettier against all ts source files
    
    * build: set npm to install exact versions, update deps
    
    * build: ng add @angular/material
    
    add material components
    
    * test: add material input and button, run tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,81 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatInput, MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+  let component: AppComponent;
+  let dom: any;
+  let fixture: ComponentFixture<AppComponent>;
+  let alertTextEl: HTMLInputElement;
+  let alertButtonEl: HTMLButtonElement;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [RouterTestingModule, FormsModule, MatButtonModule, MatInputModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    dom = fixture.nativeElement;
+    alertTextEl = dom.querySelector('input');
+    alertButtonEl = dom.querySelector('button');
+  }));
+
+  test('should exist', () => {
+    expect(component).toBeDefined();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  test('title should be Welcome to cannainflux', () => {
+    expect(component.title).toEqual('cannainflux');
   });
 
-  it(`should have as title 'cannainflux'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cannainflux');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  test('should render title', () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cannainflux app is running!');
+    expect(dom.querySelector('.content span').textContent).toContain('cannainflux app is running!');
+  });
+
+  test('should set alertText through ngModel', () => {
+    fixture.detectChanges();
+
+    alertTextEl.value = 'HelloWorld';
+    alertTextEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    expect(component.alertText).toEqual('HelloWorld');
+  });
+
+  test('should update HTML through alertText property', () => {
+    fixture.detectChanges();
+    component.alertText = 'HelloWorld!';
+    alertTextEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    expect(alertTextEl.value).toEqual(component.alertText);
+  });
+
+  test('clicking button should call handleAlert', fakeAsync(() => {
+    spyOn(component, 'handleAlert');
+
+    alertButtonEl.click();
+
+    tick();
+    expect(component.handleAlert).toHaveBeenCalled();
+  }));
+
+  test('should alert when button clicked', () => {
+    spyOn(window, 'alert');
+    component.alertText = 'Alert test';
+    alertButtonEl.click();
+    expect(window.alert).toHaveBeenCalledWith(component.alertText);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cannainflux';
+  @Input() @Output() alertText = '';
+
+  handleAlert() {
+    alert(this.alertText);
+  }
 }
