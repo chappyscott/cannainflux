a4073c3048482673925ac540a44e9644d17f3aa1
Brian Chirgwin <bchirgwin42@gmail.com>
2021-02-11 19:09:32 -0600

    feat: add jsPDF and example export (#10)
    * feat: add jsPDF and example export
    
    * Added logo
    
    Co-authored-by: Scott Chapman <scottbchapman@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output } from '@angular/core';
 
+import { jsPDF } from 'jspdf';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,16 @@ export class AppComponent {
   title = 'cannainflux';
   @Input() @Output() alertText = '';
 
+  handleCreatePDF() {
+    const doc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'in',
+      format: [8.5, 11],
+    });
+    doc.text(this.title, 1, 1);
+    doc.text(this.alertText, 1, 1.5);
+    doc.save('example.pdf');
+  }
   handleAlert() {
     alert(this.alertText);
   }
